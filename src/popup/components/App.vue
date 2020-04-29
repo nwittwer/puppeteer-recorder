6235472bcdfe50b0b1bd9fc0b845e054425473fd
Nick Wittwer <nwittwer@clincase.com>
2020-04-29 22:36:01 +0200

    Removed google analytics, checkly badge, unnecessary settings

--- src/popup/components/App.vue
+++ src/popup/components/App.vue
@@ -2,38 +2,67 @@
   <div id="puppeteer-recorder" class="recorder">
     <div class="header">
       <a href="#" @click="goHome">
-        Puppeteer recorder <span class="text-muted"><small>{{version}}</small></span>
+        Recorder
+        <span class="text-muted"
+          ><small>{{ version }}</small></span
+        >
       </a>
       <div class="left">
         <div class="recording-badge" v-show="isRecording">
           <span class="red-dot"></span>
-          {{recordingBadgeText}}
+          {{ recordingBadgeText }}
         </div>
         <a href="#" @click="toggleShowHelp" class="header-button">
-          <img src="/images/help.svg" alt="help" width="18px">
+          <img src="/images/help.svg" alt="help" width="18px" />
         </a>
         <a href="#" @click="openOptions" class="header-button">
-          <img src="/images/settings.svg" alt="settings" width="18px">
+          <img src="/images/settings.svg" alt="settings" width="18px" />
         </a>
       </div>
     </div>
     <div class="main">
       <div class="tabs" v-show="!showHelp">
-        <RecordingTab :code="code" :is-recording="isRecording" :live-events="liveEvents" v-show="!showResultsTab"/>
+        <RecordingTab
+          :code="code"
+          :is-recording="isRecording"
+          :live-events="liveEvents"
+          v-show="!showResultsTab"
+        />
         <div class="recording-footer" v-show="!showResultsTab">
-          <button class="btn btn-sm" @click="toggleRecord" :class="isRecording ? 'btn-danger' : 'btn-primary'">
-            {{recordButtonText}}
+          <button
+            class="btn btn-sm"
+            @click="toggleRecord"
+            :class="isRecording ? 'btn-danger' : 'btn-primary'"
+          >
+            {{ recordButtonText }}
           </button>
-          <button class="btn btn-sm btn-primary btn-outline-primary" @click="togglePause" v-show="isRecording">
-            {{pauseButtonText}}
+          <button
+            class="btn btn-sm btn-primary btn-outline-primary"
+            @click="togglePause"
+            v-show="isRecording"
+          >
+            {{ pauseButtonText }}
           </button>
           <a href="#" @click="showResultsTab = true" v-show="code">view code</a>
-          <checkly-badge v-if="!isRecording"></checkly-badge>
         </div>
-        <ResultsTab :code="code" :copy-link-text="copyLinkText" :restart="restart" :set-copying="setCopying" v-show="showResultsTab"/>
+        <ResultsTab
+          :code="code"
+          :copy-link-text="copyLinkText"
+          :restart="restart"
+          :set-copying="setCopying"
+          v-show="showResultsTab"
+        />
         <div class="results-footer" v-show="showResultsTab">
-          <button class="btn btn-sm btn-primary" @click="restart" v-show="code">Restart</button>
-          <a href="#" v-clipboard:copy='code' @click="setCopying" v-show="code">{{copyLinkText}}</a>
+          <button class="btn btn-sm btn-primary" @click="restart" v-show="code">
+            Restart
+          </button>
+          <a
+            href="#"
+            v-clipboard:copy="code"
+            @click="setCopying"
+            v-show="code"
+            >{{ copyLinkText }}</a
+          >
         </div>
       </div>
       <HelpTab v-show="showHelp"></HelpTab>
@@ -42,231 +71,248 @@
 </template>
 
 <script>
-  import { version } from '../../../package.json'
-  import CodeGenerator from '../../code-generator/CodeGenerator'
-  import RecordingTab from './RecordingTab.vue'
-  import ResultsTab from './ResultsTab.vue'
-  import HelpTab from './HelpTab.vue'
-  import ChecklyBadge from './ChecklyBadge.vue'
-
-  import actions from '../../models/extension-ui-actions'
+import { version } from "../../../package.json";
+import CodeGenerator from "../../code-generator/CodeGenerator";
+import RecordingTab from "./RecordingTab.vue";
+import ResultsTab from "./ResultsTab.vue";
+import HelpTab from "./HelpTab.vue";
+import actions from "../../models/extension-ui-actions";
 
 export default {
-    name: 'App',
-    components: { ResultsTab, RecordingTab, HelpTab, ChecklyBadge },
-    data () {
-      return {
-        code: '',
-        options: {},
-        showResultsTab: false,
-        showHelp: false,
-        liveEvents: [],
-        recording: [],
-        isRecording: false,
-        isPaused: false,
-        isCopying: false,
-        bus: null,
-        version
+  name: "App",
+  components: { ResultsTab, RecordingTab, HelpTab },
+  data() {
+    return {
+      code: "",
+      options: {},
+      showResultsTab: false,
+      showHelp: false,
+      liveEvents: [],
+      recording: [],
+      isRecording: false,
+      isPaused: false,
+      isCopying: false,
+      bus: null,
+      version,
+    };
+  },
+  mounted() {
+    this.loadState(() => {
+      this.trackPageView();
+      if (this.isRecording) {
+        console.debug("opened in recording state, fetching recording events");
+        this.$chrome.storage.local.get(
+          ["recording", "options"],
+          ({ recording }) => {
+            console.debug("loaded recording", recording);
+            this.liveEvents = recording;
+          }
+        );
+      }
+
+      if (!this.isRecording && this.code) {
+        this.showResultsTab = true;
+      }
+    });
+    this.bus = this.$chrome.extension.connect({ name: "recordControls" });
+  },
+  methods: {
+    toggleRecord() {
+      if (this.isRecording) {
+        this.stop();
+      } else {
+        this.start();
+      }
+      this.isRecording = !this.isRecording;
+      this.storeState();
+    },
+    togglePause() {
+      if (this.isPaused) {
+        this.bus.postMessage({ action: actions.UN_PAUSE });
+        this.isPaused = false;
+      } else {
+        this.bus.postMessage({ action: actions.PAUSE });
+        this.isPaused = true;
+      }
+      this.storeState();
+    },
+    start() {
+      this.trackEvent("Start");
+      this.cleanUp();
+      console.debug("start recorder");
+      this.bus.postMessage({ action: actions.START });
+    },
+    stop() {
+      this.trackEvent("Stop");
+      console.debug("stop recorder");
+      this.bus.postMessage({ action: actions.STOP });
+
+      this.$chrome.storage.local.get(
+        ["recording", "options"],
+        ({ recording, options }) => {
+          console.debug("loaded recording", recording);
+          console.debug("loaded options", options);
+
+          this.recording = recording;
+          const codeOptions = options ? options.code : {};
+
+          const codeGen = new CodeGenerator(codeOptions);
+          this.code = codeGen.generate(this.recording);
+          this.showResultsTab = true;
+          this.storeState();
+        }
+      );
+    },
+    restart() {
+      console.log("restart");
+      this.cleanUp();
+      this.bus.postMessage({ action: actions.CLEAN_UP });
+    },
+    cleanUp() {
+      this.recording = this.liveEvents = [];
+      this.code = "";
+      this.showResultsTab = this.isRecording = this.isPaused = false;
+      this.storeState();
+    },
+    openOptions() {
+      this.trackEvent("Options");
+      if (this.$chrome.runtime.openOptionsPage) {
+        this.$chrome.runtime.openOptionsPage();
       }
     },
-    mounted () {
-      this.loadState(() => {
-        this.trackPageView()
-        if (this.isRecording) {
-          console.debug('opened in recording state, fetching recording events')
-          this.$chrome.storage.local.get(['recording', 'options'], ({ recording }) => {
-            console.debug('loaded recording', recording)
-            this.liveEvents = recording
-          })
-        }
-
-        if (!this.isRecording && this.code) {
-          this.showResultsTab = true
-        }
-      })
-      this.bus = this.$chrome.extension.connect({ name: 'recordControls' })
-    },
-    methods: {
-      toggleRecord () {
-        if (this.isRecording) {
-          this.stop()
-        } else {
-          this.start()
-        }
-        this.isRecording = !this.isRecording
-        this.storeState()
-      },
-      togglePause () {
-        if (this.isPaused) {
-          this.bus.postMessage({ action: actions.UN_PAUSE })
-          this.isPaused = false
-        } else {
-          this.bus.postMessage({ action: actions.PAUSE })
-          this.isPaused = true
-        }
-        this.storeState()
-      },
-      start () {
-        this.trackEvent('Start')
-        this.cleanUp()
-        console.debug('start recorder')
-        this.bus.postMessage({ action: actions.START })
-      },
-      stop () {
-        this.trackEvent('Stop')
-        console.debug('stop recorder')
-        this.bus.postMessage({ action: actions.STOP })
-
-        this.$chrome.storage.local.get(['recording', 'options'], ({ recording, options }) => {
-          console.debug('loaded recording', recording)
-          console.debug('loaded options', options)
-
-          this.recording = recording
-          const codeOptions = options ? options.code : {}
-
-          const codeGen = new CodeGenerator(codeOptions)
-          this.code = codeGen.generate(this.recording)
-          this.showResultsTab = true
-          this.storeState()
-        })
-      },
-      restart () {
-        console.log('restart')
-        this.cleanUp()
-        this.bus.postMessage({ action: actions.CLEAN_UP })
-      },
-      cleanUp () {
-        this.recording = this.liveEvents = []
-        this.code = ''
-        this.showResultsTab = this.isRecording = this.isPaused = false
-        this.storeState()
-      },
-      openOptions () {
-        this.trackEvent('Options')
-        if (this.$chrome.runtime.openOptionsPage) {
-          this.$chrome.runtime.openOptionsPage()
-        }
-      },
-      loadState (cb) {
-        this.$chrome.storage.local.get(['controls', 'code', 'options'], ({ controls, code, options }) => {
+    loadState(cb) {
+      this.$chrome.storage.local.get(
+        ["controls", "code", "options"],
+        ({ controls, code, options }) => {
           if (controls) {
-            this.isRecording = controls.isRecording
-            this.isPaused = controls.isPaused
+            this.isRecording = controls.isRecording;
+            this.isPaused = controls.isPaused;
           }
 
           if (code) {
-            this.code = code
+            this.code = code;
           }
 
           if (options) {
-            this.options = options
+            this.options = options;
           }
-          cb()
-        })
-      },
-      storeState () {
-        this.$chrome.storage.local.set({
-          code: this.code,
-          controls: {
-            isRecording: this.isRecording,
-            isPaused: this.isPaused
-          }
-        })
-      },
-      setCopying () {
-        this.trackEvent('Copy')
-        this.isCopying = true
-        setTimeout(() => { this.isCopying = false }, 1500)
-      },
-      goHome () {
-        this.showResultsTab = false
-        this.showHelp = false
-      },
-      toggleShowHelp () {
-        this.trackEvent('Help')
-        this.showHelp = !this.showHelp
-      },
-      trackEvent (event) {
-        if (this.options && this.options.extension && this.options.extension.telemetry) {
-          if (window._gaq) window._gaq.push(['_trackEvent', event, 'clicked'])
+          cb();
         }
-      },
-      trackPageView () {
-        if (this.options && this.options.extension && this.options.extension.telemetry) {
-          if (window._gaq) window._gaq.push(['_trackPageview'])
-        }
+      );
+    },
+    storeState() {
+      this.$chrome.storage.local.set({
+        code: this.code,
+        controls: {
+          isRecording: this.isRecording,
+          isPaused: this.isPaused,
+        },
+      });
+    },
+    setCopying() {
+      this.trackEvent("Copy");
+      this.isCopying = true;
+      setTimeout(() => {
+        this.isCopying = false;
+      }, 1500);
+    },
+    goHome() {
+      this.showResultsTab = false;
+      this.showHelp = false;
+    },
+    toggleShowHelp() {
+      this.trackEvent("Help");
+      this.showHelp = !this.showHelp;
+    },
+    trackEvent(event) {
+      if (
+        this.options &&
+        this.options.extension &&
+        this.options.extension.telemetry
+      ) {
+        if (window._gaq) window._gaq.push(["_trackEvent", event, "clicked"]);
       }
     },
-    computed: {
-      recordingBadgeText () {
-        return this.isPaused ? 'paused' : 'recording'
-      },
-      recordButtonText () {
-        return this.isRecording ? 'Stop' : 'Record'
-      },
-      pauseButtonText () {
-        return this.isPaused ? 'Resume' : 'Pause'
-      },
-      copyLinkText () {
-        return this.isCopying ? 'copied!' : 'copy to clipboard'
+    trackPageView() {
+      if (
+        this.options &&
+        this.options.extension &&
+        this.options.extension.telemetry
+      ) {
+        if (window._gaq) window._gaq.push(["_trackPageview"]);
       }
-    }
-}
+    },
+  },
+  computed: {
+    recordingBadgeText() {
+      return this.isPaused ? "paused" : "recording";
+    },
+    recordButtonText() {
+      return this.isRecording ? "Stop" : "Record";
+    },
+    pauseButtonText() {
+      return this.isPaused ? "Resume" : "Pause";
+    },
+    copyLinkText() {
+      return this.isCopying ? "copied!" : "copy to clipboard";
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  @import "~styles/_animations.scss";
-  @import "~styles/_variables.scss";
-  @import "~styles/_mixins.scss";
-  .recorder {
-    font-size: 14px;
+@import "~styles/_animations.scss";
+@import "~styles/_variables.scss";
+@import "~styles/_mixins.scss";
+.recorder {
+  font-size: 14px;
 
-    .header {
-      @include header();
+  .header {
+    @include header();
 
-      a {
-        color: $gray-dark;
-      }
-
-      .left {
-        margin-left: auto;
-        display: flex;
-        justify-content: flex-start;
-        align-items: center;
-
-        .recording-badge {
-          color: $brand-danger;
-          .red-dot {
-            height: 9px;
-            width: 9px;
-            background-color: $brand-danger;
-            border-radius: 50%;
-            display: inline-block;
-            margin-right: .4rem;
-            vertical-align: middle;
-            position: relative;
-          }
-        }
-
-        .header-button {
-          margin-left: $spacer;
-          img {
-            vertical-align: middle;
-          }
-        }
-      }
+    a {
+      color: $gray-dark;
     }
 
-    .recording-footer {
-      @include footer();
-      img {
-        margin-left: 8px;
-        width: 80px;
-        vertical-align: middle;
+    .left {
+      margin-left: auto;
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+
+      .recording-badge {
+        color: $brand-danger;
+        .red-dot {
+          height: 9px;
+          width: 9px;
+          background-color: $brand-danger;
+          border-radius: 50%;
+          display: inline-block;
+          margin-right: 0.4rem;
+          vertical-align: middle;
+          position: relative;
+        }
       }
-    }
-    .results-footer {
-      @include footer()
+
+      .header-button {
+        margin-left: $spacer;
+        img {
+          vertical-align: middle;
+        }
+      }
     }
   }
+
+  .recording-footer {
+    @include footer();
+    img {
+      margin-left: 8px;
+      width: 80px;
+      vertical-align: middle;
+    }
+  }
+  .results-footer {
+    @include footer();
+  }
+}
 </style>
--- src/popup/components/RecordingTab.vue
+++ src/popup/components/RecordingTab.vue
@@ -2,23 +2,29 @@
   <div class="tab recording-tab">
     <div class="content">
       <div class="empty" v-show="!isRecording">
-        <img src="/images/Desert.svg" alt="desert" width="78px">
+        <img src="/images/Desert.svg" alt="desert" width="78px" />
         <h3>No recorded events yet</h3>
         <p class="text-muted">Click record to begin</p>
-        <div class="nag-cta" v-show="!isRecording">
-          <a href="https://github.com/checkly/puppeteer-recorder/issues/83" target="_blank">⚡️Updates on the roadmap of Puppeteer Recorder →</a>
-        </div>
       </div>
       <div class="events" v-show="isRecording">
-        <p class="text-muted text-center loading" v-show="liveEvents.length === 0">Waiting for events</p>
+        <p
+          class="text-muted text-center loading"
+          v-show="liveEvents.length === 0"
+        >
+          Waiting for events
+        </p>
         <ul class="event-list">
-          <li v-for="(event, index) in liveEvents" :key="index" class="event-list-item">
-            <div class="event-label">
-              {{index + 1}}.
-            </div>
+          <li
+            v-for="(event, index) in liveEvents"
+            :key="index"
+            class="event-list-item"
+          >
+            <div class="event-label">{{ index + 1 }}.</div>
             <div class="event-description">
-              <div class="event-action">{{event.action}}</div>
-              <div class="event-props text-muted">{{event.selector || parseEventValue(event)}}</div>
+              <div class="event-action">{{ event.action }}</div>
+              <div class="event-props text-muted">
+                {{ event.selector || parseEventValue(event) }}
+              </div>
             </div>
           </li>
         </ul>
@@ -27,93 +33,97 @@
   </div>
 </template>
 <script>
-  export default {
-    name: 'RecordingTab',
-    props: {
-      isRecording: { type: Boolean, default: false },
-      liveEvents: { type: Array, default: () => { return [] } }
+export default {
+  name: "RecordingTab",
+  props: {
+    isRecording: { type: Boolean, default: false },
+    liveEvents: {
+      type: Array,
+      default: () => {
+        return [];
+      },
     },
-    methods: {
-      parseEventValue (event) {
-        if (event.action === 'viewport*') return `width: ${event.value.width}, height: ${event.value.height}`
-        if (event.action === 'goto*') return event.href
-        if (event.action === 'navigation*') return ''
-      }
-    }
-  }
+  },
+  methods: {
+    parseEventValue(event) {
+      if (event.action === "viewport*")
+        return `width: ${event.value.width}, height: ${event.value.height}`;
+      if (event.action === "goto*") return event.href;
+      if (event.action === "navigation*") return "";
+    },
+  },
+};
 </script>
 <style lang="scss" scoped>
-  @import "~styles/_animations.scss";
-  @import "~styles/_variables.scss";
+@import "~styles/_animations.scss";
+@import "~styles/_variables.scss";
 
+.recording-tab {
+  .content {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    min-height: 200px;
+    .empty {
+      padding: $spacer;
+      text-align: center;
+    }
 
-  .recording-tab {
-    .content {
-      display:flex;
-      flex-direction:column;
-      height:100%;
-      min-height: 200px;
-      .empty {
-        padding: $spacer;
-        text-align: center;
+    .events {
+      max-height: $max-content-height;
+      flex: 1;
+      height: 100%;
+      overflow: auto;
+      display: flex;
+      flex-direction: column-reverse;
+
+      .loading:after {
+        content: ".";
+        animation: dots 1s steps(5, end) infinite;
+        animation-delay: 1.5s;
+        margin-bottom: auto;
       }
 
-      .events {
-        max-height: $max-content-height;
-        flex: 1;
-        height:100%;
-        overflow: auto;
-        display: flex;
-        flex-direction: column-reverse;
+      .event-list {
+        list-style-type: none;
+        padding: 0;
+        margin: 0;
 
-        .loading:after {
-          content: '.';
-          animation: dots 1s steps(5, end) infinite;
-          animation-delay: 1.5s;
-          margin-bottom: auto;
-        }
+        .event-list-item {
+          padding: 12px;
+          font-size: 12px;
+          border-top: 1px solid $gray-light;
+          display: flex;
+          flex: 1 1 auto;
+          height: 32px;
 
-        .event-list {
-          list-style-type: none;
-          padding: 0;
-          margin: 0;
+          .event-label {
+            vertical-align: top;
+            margin-right: $spacer;
+          }
 
-          .event-list-item {
-            padding: 12px;
-            font-size: 12px;
-            border-top: 1px solid $gray-light;
-            display: flex;
-            flex: 1 1 auto;
-            height: 32px;
+          .event-description {
+            margin-right: auto;
+            display: inline-block;
 
-            .event-label {
-              vertical-align: top;
-              margin-right: $spacer;
+            .event-action {
+              font-weight: bold;
             }
 
-            .event-description {
-              margin-right: auto;
-              display: inline-block;
-
-              .event-action {
-                font-weight: bold;
-              }
-
-              .event-props {
-                white-space: pre;
-              }
+            .event-props {
+              white-space: pre;
             }
-
           }
         }
       }
     }
-    .nag-cta {
-      a {
-        color: $pink;
-        font-size: 80%;
-        font-weight: 500;
-      }
+  }
+  .nag-cta {
+    a {
+      color: $pink;
+      font-size: 80%;
+      font-weight: 500;
     }
   }
+}
 </style>
